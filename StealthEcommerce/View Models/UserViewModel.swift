a5ed43f9a6f561f02e71304423af8524c69251c8
ShamithR1000 <Shamith.Ramdhani2+mtn@mtn.com>
2025-07-11 07:19:22 +0200

    Updated UserViewModel to use one service call function

--- StealthEcommerce/View Models/UserViewModel.swift
+++ StealthEcommerce/View Models/UserViewModel.swift
@@ -12,43 +12,15 @@ class UserViewModel: ObservableObject {
     @Published var message: String?
     
     func createUser(user: User) {
-        guard let url = URL(string: "http://localhost:3000/api/users") else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        do {
-            let jsonData = try JSONEncoder().encode(user)
-            request.httpBody = jsonData
-        } catch {
-            print("Encoding error:\(error)")
-            return
-        }
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print("Error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let data = data else {
-                print("Network error\(error?.localizedDescription ?? "Unknown")")
-                return
-            }
-            
-            if let responseString = String(data: data, encoding: .utf8) {
-                DispatchQueue.main.async {
-                    self.message = "Response: \(responseString)"
-                }
-            }
-        }.resume()
+        networkRequest(url: "http://localhost:3000/api/users", user: user)
     }
     
     func loginUser(user: User) {
-        
-        guard let url = URL(string: "http://localhost:3000/api/users/login") else { return }
+        networkRequest(url:  "http://localhost:3000/api/users/login", user: user)
+    }
+    
+    private func networkRequest(url: String, user: User) {
+        guard let url = URL(string: url) else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
